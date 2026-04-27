47ac8ca5a58b685f73e16e8d74efc5afce1a7a6b
Shekhar Sahu <shekharsahu1020@gmail.com>
2026-04-28 00:24:54 +0530

    Added delete feature and creation limit

--- backend/src/modules/projects/projects.service.ts
+++ backend/src/modules/projects/projects.service.ts
@@ -1,9 +1,20 @@
 import { NextFunction, Request, Response } from "express"
-import { createProjectRepo, getAllProjectRepo } from "./projects.repository";
+import { createProjectRepo, deleteProjectRepo, getAllProjectRepo, getProjectCount } from "./projects.repository";
 import { AppError } from "../../utility/errorClass";
+import { error } from "node:console";
 
 export const createProjectService = async (userId: string, projectName: string, description: string) => {
-    console.log("createProjectService called")
+
+    // Check if project limit is reached
+    const { max_projects, used_projects }: { max_projects: number; used_projects: number } = await getProjectCount(userId);
+
+    console.log("project data", max_projects, "project data", used_projects);
+
+
+    if (Number(used_projects) >= max_projects) {
+        throw new Error("Project creation limit reached. Please upgrade your plan.");
+    }
+
     try {
         return await createProjectRepo(userId, projectName, description);
     } catch (error: any) {
@@ -13,7 +24,6 @@ export const createProjectService = async (userId: string, projectName: string, 
 
 
 export const getAllProjectService = async (userId: string) => {
-    console.log("createProjectService called")
     try {
         return await getAllProjectRepo(userId);
     } catch (error: any) {
@@ -21,3 +31,10 @@ export const getAllProjectService = async (userId: string) => {
     }
 }
 
+export const deleteProjectService = async (projectId: string | string[]) => {
+    try {
+        return await deleteProjectRepo(projectId);
+    } catch (error: any) {
+        throw error;
+    }
+}
